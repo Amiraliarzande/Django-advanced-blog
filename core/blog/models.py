ae0f17e0458bbe30fa06a724760c57c5be12c3bb
Amiraliarzande <amiraliarzande8@gmail.com>
2025-09-21 18:26:38 +0330

    Refactor account app: delete old files and implement custom user model with manager

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class Post (models.Model):
+
+    image = models.ImageField(null=True, blank=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.ForeignKey("Category",on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+class Category (models.Model):
+
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
--- core/core/settings.py
+++ core/core/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
-    'blog',
+    'accounts',
+    # 'blog',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager configuration
+AUTH_USER_MODEL = "accounts.User"
