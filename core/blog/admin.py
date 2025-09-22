a4433d3953870893df3eca77665cff5c8cc258eb
Amiraliarzande <amiraliarzande8@gmail.com>
2025-09-22 19:10:34 +0330

    Add blog app models and admin configuration; register Post and Category models

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Category
 
 # Register your models here.
+
+class blogAdmin (admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'status', 'created_at', 'published_at')
+    list_filter = ('status', 'category', 'author')
+    search_fields = ('title', 'content')
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    filter_horizontal = ()
+    fieldsets = ()
+
+admin.site.register(Post, blogAdmin)
+admin.site.register(Category)
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from accounts.models import User
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+# getting the user model
+User = get_user_model()
 
 class Post (models.Model):
 
