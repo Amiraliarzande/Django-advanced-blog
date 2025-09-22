ab175b44d2854b8395d663438ffcf63834a4ae75
Amiraliarzande <amiraliarzande8@gmail.com>
2025-09-22 17:59:01 +0330

    Add Profile model and update User admin registration; modify user is_active field default

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import  (BaseUserManager,AbstractBaseUser, PermissionsMixin)
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
@@ -43,3 +45,20 @@ class User (AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField(null=True, blank=True)
+    desciption = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
