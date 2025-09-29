1a48e35b9cefe05a7e82485abf65c42d33453d1e
Amiraliarzande <amiraliarzande8@gmail.com>
2025-09-29 18:08:10 +0330

    Refactor PostSerializer to use ModelSerializer and update ApiPostList for improved handling of GET and POST requests

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
+from ...models import Post
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=200)
-    author = serializers.CharField(max_length=100)
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=200)
+#     author = serializers.CharField(max_length=100)
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'author', 'content', 'created_at', 'updated_at', 'status']
+
