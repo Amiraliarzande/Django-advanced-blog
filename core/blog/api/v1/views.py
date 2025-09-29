5f82f49b06f1fdbc0644cf8d62f436b2a632aa67
Amiraliarzande <amiraliarzande8@gmail.com>
2025-09-29 16:06:54 +0330

    Implement API overview to return post details and add PostSerializer (testing)

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from ...models import Post
 
 @api_view()
 def apiOverview(request, pk):
-    return Response({"name": "ali", "pk": pk})
+    post = Post.objects.get(id=pk)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
