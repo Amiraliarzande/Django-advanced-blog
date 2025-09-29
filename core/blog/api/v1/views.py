5d9555aac99345a3189529ea58093a1fe1a1c4eb
Amiraliarzande <amiraliarzande8@gmail.com>
2025-09-29 16:31:21 +0330

    Refactor apiOverview to use get_object_or_404 for improved error handling

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,9 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def apiOverview(request, pk):
-    post = Post.objects.get(id=pk)
+    post = get_object_or_404(Post, id=pk)
     serializer = PostSerializer(post)
     return Response(serializer.data)
