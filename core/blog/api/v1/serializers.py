4353a7b60d17f6425fb030739700cb2d46ca69af
Amiraliarzande <amiraliarzande8@gmail.com>
2025-10-02 20:30:46 +0330

    Refactor PostSerializer to ensure proper context handling for category serialization and author assignment during creation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from ...models import Post, Category
+from accounts.models import Profile
 
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField(read_only=True)
@@ -21,8 +22,10 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title','category', 'author', 'content','snippet_content','status' ,'created_at', 'updated_at','published_at','absolute_url']
+        read_only_fields = ['author']
 
     def get_absolute_url(self, obj):
+        # Generate absolute URL for the post
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
     
@@ -38,6 +41,10 @@ class PostSerializer(serializers.ModelSerializer):
             # If 'pk' is not present, remove 'content'
             rep.pop('content')
 
-        rep['category'] = PostCreateSerializer(instance.category).data
+        rep['category'] = PostCreateSerializer(instance.category, context={'request': request}).data # Serialize category field
         return rep
-
+    def create(self, validated_data):
+        # Automatically set the author to the current user
+        validated_data['author'] = Profile.objects.get(user__id=self.context.get('request').user.id)
+        return super().create(validated_data)
+    
