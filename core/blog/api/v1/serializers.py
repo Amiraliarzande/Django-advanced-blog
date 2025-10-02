4f30099a0d8cfd7184e3ac7f619154eaca3bf7b6
Amiraliarzande <amiraliarzande8@gmail.com>
2025-10-02 17:38:34 +0330

    Refactor PostSerializer to include category field and enhance to_representation method for better response customization

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -6,21 +6,38 @@ from ...models import Post, Category
 #     title = serializers.CharField(max_length=200)
 #     author = serializers.CharField(max_length=100)
 
-class PostSerializer(serializers.ModelSerializer):
-
-    snippet_content = serializers.CharField(source='snippet_api_content', read_only=True)
-    absolute_url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'author', 'content','snippet_content','status' ,'created_at', 'updated_at','published_at','absolute_url']
-
-    def get_absolute_url(self, obj):
-        request = self.context.get('request')
-        return request.build_absolute_uri(obj.pk)
-
 class PostCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
         fields = ['id','name']
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    snippet_content = serializers.CharField(source='snippet_api_content', read_only=True)
+    absolute_url = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title','category', 'author', 'content','snippet_content','status' ,'created_at', 'updated_at','published_at','absolute_url']
+
+    def get_absolute_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+    
+    def to_representation(self, instance):
+        # Customize representation based on presence of 'pk' in URL kwargs
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            # If 'pk' is present, remove 'snippet_content' and 'absolute_url'
+            rep.pop('snippet_content')
+            rep.pop('absolute_url')
+        else:
+            # If 'pk' is not present, remove 'content'
+            rep.pop('content')
+
+        rep['category'] = PostCreateSerializer(instance.category).data
+        return rep
+
