76082a75d240eca796c1d9745f23474284e041de
Amiraliarzande <amiraliarzande8@gmail.com>
2025-10-02 16:28:24 +0330

    Add absolute_url field to PostSerializer with method for generating full URL

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,10 +9,15 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
 
     snippet_content = serializers.CharField(source='snippet_api_content', read_only=True)
+    absolute_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'author', 'content','snippet_content','status' ,'created_at', 'updated_at','published_at']
+        fields = ['id', 'title', 'author', 'content','snippet_content','status' ,'created_at', 'updated_at','published_at','absolute_url']
+
+    def get_absolute_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 
 class PostCreateSerializer(serializers.ModelSerializer):
 
