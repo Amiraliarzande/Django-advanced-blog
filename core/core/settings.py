042735b56413411d0f97a1a1b0df468dfc2561cc
Amiraliarzande <amiraliarzande8@gmail.com>
2025-10-06 17:17:20 +0330

    Add Token Authentication to Django REST Framework settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'drf_yasg',
+    'rest_framework.authtoken',
     
     'accounts',
     'blog',
@@ -142,6 +143,11 @@ AUTH_USER_MODEL = "accounts.User"
 # Django REST Framework configuration
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
 
