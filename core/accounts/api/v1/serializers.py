c59370d74629cdd0ca50427f0553b6721818f84b
Amiraliarzande <amiraliarzande8@gmail.com>
2025-10-07 19:04:41 +0330

    Add custom authentication token serializer and login view

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.password_validation import validate_password
 from ...models import User  # Assuming User model is imported from appropriate location
+
+
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(write_only=True)
@@ -27,8 +32,41 @@ class RegistrationSerializer(serializers.ModelSerializer):
         validated_data.pop('password1')
         user = User.objects.create_user(**validated_data)
         return user
+    
+class CustomAuthTokenSerializer(serializers.Serializer):
+    """
+    Serializer for the authentication endpoint.
+    """
+    email = serializers.CharField(
+        label=_("Email"),
+        write_only=True
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
 
+    def validate(self, attrs):
+        username = attrs.get('email')
+        password = attrs.get('password')
 
-        
+        if username and password:
+            # احراز هویت کاربر
+            user = authenticate(
+                request=self.context.get('request'),
+                username=username,
+                password=password
+            )
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs    
 
     
--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -5,4 +5,5 @@ app_name = 'api_v1'
 
 urlpatterns = [
     path('registration/', views.RegistrationView.as_view(), name='registration'),
+    path('login/', views.CustomAuthToken.as_view(), name='login'),
 ]
